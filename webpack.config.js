d773c4d38a67479adf06cff0a23bba3f793f167b
halfcadence <andrew@halfcadence.io>
2019-08-02 16:24:56 -0700

    extracts css to file with unique hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,55 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (_env, argv) => {
+  const devMode = argv.mode !== 'production';
+
+  return {
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [{
-          loader: "style-loader"
-        }, {
-          loader: "css-loader"
-        }, {
-          loader: "sass-loader",
-        }]
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ],
-  output: {
-    filename: 'bundle.[chunkhash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-};
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: [{
+            loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader
+          },       
+          {
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader",
+          }]
+        }
+      ]
+    },
+    output: {
+      filename: 'bundle.[chunkhash].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+  };
+}
