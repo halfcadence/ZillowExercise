2307491031f7f387033fe1569791d993e99745b9
halfcadence <andrew@halfcadence.io>
2019-08-05 09:01:04 -0700

    basic gallery

--- src/index.js
+++ src/index.js
@@ -1,13 +1,131 @@
 import React from 'react';
 import { render } from 'react-dom';
+import styled from 'styled-components';
+import JustifiedLayout from 'justified-layout';
+
+import img0 from './assets/0.jpg';
+import img1 from './assets/1.jpg';
+import img2 from './assets/2.jpg';
+import img3 from './assets/3.jpg';
+import img4 from './assets/4.jpg';
+import img5 from './assets/5.jpg';
+import img6 from './assets/6.jpg';
+import img7 from './assets/7.jpg';
+import img8 from './assets/8.jpg';
+import img9 from './assets/9.jpg';
+import img10 from './assets/10.jpg';
+import img11 from './assets/11.jpg';
+
 import './index.scss';
-import Test from './test.jpg';
-import Microphone from './microphone.svg'
+
+const tabletBreakpoint = 600
+const desktopBreakpoint = 960
+
+const images = [
+  {
+    src: img0,
+    aspectRatio: 5268 / 3512,
+    caption: "An Image"
+  },
+  {
+    src: img1,
+    aspectRatio: 5004 / 3336,
+    caption: "An Image"
+  },
+  {
+    src: img2,
+    aspectRatio: 6000 / 4000,
+    caption: "An Image"
+  },
+  {
+    src: img3,
+    aspectRatio: 3264 / 2176,
+    caption: "An Image"
+  },
+
+  {
+    src: img4,
+    aspectRatio: 2873 / 1915,
+    caption: "An Image"
+  },
+  {
+    src: img5,
+    aspectRatio: 2579 / 1720,
+    caption: "An Image"
+  },
+  {
+    src: img6,
+    aspectRatio: 6000 / 4000,
+    caption: "An Image"
+  },
+  {
+    src: img7,
+    aspectRatio: 2971 / 1981,
+    caption: "An Image"
+  },
+  {
+    src: img8,
+    aspectRatio: 4621 / 3081,
+    caption: "An Image"
+  },
+  {
+    src: img9,
+    aspectRatio: 4928 / 3264,
+    caption: "An Image"
+  },
+  {
+    src: img10,
+    aspectRatio: 6000 / 4000,
+    caption: "An Image"
+  },
+  {
+    src: img11,
+    aspectRatio: 3469 / 2313,
+    caption: "An Image"
+  }
+]
+
 const App = () => (
   <div>
-    Hello Zillow
-    <img src={Microphone}></img>
+    <Gallery/>
+    {/* <img src={Test}></img> */}
     </div>
 )
 
+const Gallery = () => {
+  const screenWidth = window.innerWidth;
+  const padding = screenWidth < tabletBreakpoint ? 10 : screenWidth < desktopBreakpoint ? 25 : 50
+
+  const config = {
+    containerPadding: padding,
+    containerWidth: screenWidth
+  }
+
+  const layoutGeometry = JustifiedLayout(images.map(img => img.aspectRatio), config)
+  console.log(layoutGeometry)
+  return <StyledGallery height={layoutGeometry.containerHeight}>
+    {images.map((img, index) =>
+      <StyledImg
+        src={img.src}
+        height={layoutGeometry.boxes[index].height}
+        top={layoutGeometry.boxes[index].top}
+        left={layoutGeometry.boxes[index].left}
+        
+        key={index}
+        >
+      </StyledImg>
+    )}
+    </StyledGallery>
+}
+
+const StyledGallery = styled.div`
+  height: ${props => props.height}px;
+`
+const StyledImg = styled.img`
+  position: absolute;
+  height: ${props => props.height}px;
+  top: ${props => props.top}px;
+  left: ${props => props.left}px;
+  background-color: #F5F5F5;
+`
 render(<App />, document.getElementById('root'))
